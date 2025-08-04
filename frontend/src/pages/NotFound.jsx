d2bcc6cd7819323189cd7451ca3000517b491f49
Timothy Glitch <timsahm2009@gmx.de>
2025-08-04 15:54:49 +0200

    first commit

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+	return (
+		<>
+			<h1>404 Page Not Found</h1>
+			<p>The Page you are looking at is not existing on this Website!</p>
+		</>
+	);
+}
+export default NotFound;
